08a1668cff952818470e988779d264ff20f3a7b8
Ryan Kaoukeb <ryan.kaoukeb@etu.emse.fr>
2021-11-23 11:33:35 +0100

    Somme bullshit added by TangoAplha

---
+++ README.txt
@@ -0,0 +1,4 @@
+# AppCartel2022_WebSite
+
+Nothing is required to run the code. VueJs is 
+imported directly from the web
--- index.js
+++ index.js
@@ -6,3 +6,19 @@ var app = new Vue({
   })
 
 //app.message="shit"
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+      message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+  })
+
+  var app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: true
+    }
+  })  
+
+  app3.seen = false
