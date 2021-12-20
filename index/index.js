7b4fac0a0adb29114175f8c320a901dccb2b0a78
sophie.curinier <sophie.curinier@etu.emse.fr>
2021-12-20 10:54:06 +0100

    CSS

--- index/index.js
+++ index/index.js
@@ -1,24 +1,14 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Sophie!'
-    }
-  })
+var slideIndex = 0;
+carousel();
 
-
-
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-      message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-  })
-
-  var app3 = new Vue({
-    el: '#app-3',
-    data: {
-      seen: true
-    }
-  })  
-
-  
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 4000); // Change image every 2 seconds
+}
