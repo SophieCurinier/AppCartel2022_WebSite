af33c658064f1e48fddc28d5ca3719d9555ddc3d
Pierre Maindron <pierre.maindron@etu.emse.fr>
2021-12-20 21:31:42 +0100

    Ajout respos délégations

--- contacts.js
+++ contacts.js
@@ -1,19 +1,20 @@
-
-        var slideIndex = 1;
-        showDivs(slideIndex);
+<script>
+  var slideIndex = 1;
+  showDivs(slideIndex);
         
-        function plusDivs(n) {
-          showDivs(slideIndex += n);
-        }
+  function plusDivs(n) {
+    showDivs(slideIndex += n)
+  }
         
-        function showDivs(n) {
-          var i;
-          var x = document.getElementsByClassName("Contacts");
-          if (n > x.length) {slideIndex = 1}
-          if (n < 1) {slideIndex = x.length}
-          for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";  
-          }
-          x[slideIndex-1].style.display = "block";  
-        }
-        
+  function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("Contacts");
+    if (n > x.length) {slideIndex = 1};
+    if (n < 1) {slideIndex = x.length};
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    x[slideIndex-1].style.display = "block";  
+  }
+  
+  </script>
