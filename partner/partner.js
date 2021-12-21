9ce7829b214aba31997ff18a1078ada40d0f390b
sophie.curinier <sophie.curinier@etu.emse.fr>
2021-12-21 11:50:32 +0100

    Partenaires CSS

--- partner/partner.js
+++ partner/partner.js
@@ -1,17 +1,31 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+var slideIndex = 0;
+carousel();
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
+function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";  
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 5000); // Change image every 5 seconds
+}
+
+var slideIndex_partner = 0;
+carousel_partner();
+function carousel_partner() {
+  var i;
+  var x = document.getElementsByClassName("partner_mySlides");
+  var y = document.getElementsByClassName("partner_mySlides_background");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+    y[i].style.display = "none";
+  }
+  slideIndex_partner++;
+  if (slideIndex_partner > x.length) {slideIndex_partner = 1}
+  x[slideIndex_partner-1].style.display = "block";
+  y[slideIndex_partner-1].style.display = "block";
+  setTimeout(carousel_partner, 5000); // Change image every 5 seconds
 }
